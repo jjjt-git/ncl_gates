7b767c7e94b09486fde931aad56f9f480ca86ac5
Jacob Tilger <jacob.2015@tilger.net>
2025-08-14 17:06:53 +0200

    renamed library to ncl_gates

--- NCL_completion_recursion_unit.vhd
+++ NCL_completion_recursion_unit.vhd
@@ -7,7 +7,7 @@
 ----------------------------------------------------------------------------------
 
 library IEEE;
-library ncl_xilinx_gates;
+library ncl_gates;
 
 use IEEE.STD_LOGIC_1164.ALL;
 
@@ -25,15 +25,15 @@ architecture Structural of NCL_completion_recursion_unit is
 begin
 
     complete_4 : if width = 4 generate 
-        single_gate : entity ncl_xilinx_gates.TH44 port map (A => vector_input(3), B => vector_input(2), C => vector_input(1), D => vector_input(0), Z => result_output);
+        single_gate : entity ncl_gates.TH44 port map (A => vector_input(3), B => vector_input(2), C => vector_input(1), D => vector_input(0), Z => result_output);
     end generate;
         
     complete_3 : if width = 3 generate 
-        single_gate : entity ncl_xilinx_gates.TH33 port map (A => vector_input(2), B => vector_input(1), C => vector_input(0), Z => result_output);
+        single_gate : entity ncl_gates.TH33 port map (A => vector_input(2), B => vector_input(1), C => vector_input(0), Z => result_output);
     end generate;
     
     complete_2 : if width = 2 generate 
-        single_gate : entity ncl_xilinx_gates.TH22 port map (A => vector_input(1), B => vector_input(0), Z => result_output);
+        single_gate : entity ncl_gates.TH22 port map (A => vector_input(1), B => vector_input(0), Z => result_output);
     end generate;
     
     complete_1 : if width = 1 generate 
@@ -43,7 +43,7 @@ begin
     continue_recursion : if (width > 4) generate
      
         four_input_gates : for i in (width/4) downto 1 generate
-            unit: entity ncl_xilinx_gates.TH44 port map (   A => vector_input( width-( ((width/4)-i)*4 )-1), 
+            unit: entity ncl_gates.TH44 port map (   A => vector_input( width-( ((width/4)-i)*4 )-1), 
                                                             B => vector_input( width-( ((width/4)-i)*4 )-2), 
                                                             C => vector_input( width-( ((width/4)-i)*4 )-3), 
                                                             D => vector_input( width-( ((width/4)-i)*4 )-4), 
@@ -53,14 +53,14 @@ begin
         -- ----------
             
         three_input_remainder_gate: if (width mod 4 = 3) generate
-            three_input_unit: entity ncl_xilinx_gates.TH33 port map (  A => vector_input(2), 
+            three_input_unit: entity ncl_gates.TH33 port map (  A => vector_input(2), 
                                                                        B => vector_input(1), 
                                                                        C => vector_input(0),
                                                                        Z => intermediate_signal(0));
         end generate;
         
         two_input_remainder_gate: if (width mod 4 = 2) generate
-            two_input_unit: entity ncl_xilinx_gates.TH22 port map (   A => vector_input(1), 
+            two_input_unit: entity ncl_gates.TH22 port map (   A => vector_input(1), 
                                                                        B => vector_input(0),
                                                                        Z => intermediate_signal(0));
         end generate;
@@ -72,14 +72,14 @@ begin
         -- ----------
         
         recursion_with_remainer : if (width mod 4 /= 0) generate    
-            recursive_unit: entity ncl_xilinx_gates.NCL_completion_recursion_unit 
+            recursive_unit: entity ncl_gates.NCL_completion_recursion_unit 
                     generic map ( width => width/4+1 )
                     port map (  vector_input => intermediate_signal,
                                 result_output => result_output );
         end generate;
         
         recursion_no_remainer: if (width mod 4 = 0) generate                    
-        recursive_unit: entity ncl_xilinx_gates.NCL_completion_recursion_unit 
+        recursive_unit: entity ncl_gates.NCL_completion_recursion_unit 
                 generic map ( width => width/4 )
                 port map (  vector_input => intermediate_signal((width/4) downto 1),
                             result_output => result_output );
--- completion_unit.vhd
+++ completion_unit.vhd
@@ -6,7 +6,7 @@
 ----------------------------------------------------------------------------------
 
 library IEEE;
-library ncl_xilinx_gates;
+library ncl_gates;
 
 use IEEE.STD_LOGIC_1164.ALL;
 
@@ -24,7 +24,7 @@ architecture Structural of completion_unit is
 begin
 
     single_completion_chain: for i in bit_width-1 downto 0 generate
-        single_completion_gate : entity ncl_xilinx_gates.TH12 port map (
+        single_completion_gate : entity ncl_gates.TH12 port map (
             A => input(2*i+1),
             B => input(2*i),
             Z => single_completion(i)
@@ -33,7 +33,7 @@ begin
     
     single_completion_negated <= not single_completion;
     
-    completion_recursion : entity ncl_xilinx_gates.NCL_completion_recursion_unit
+    completion_recursion : entity ncl_gates.NCL_completion_recursion_unit
     generic map ( width => bit_width )
     port map (  vector_input => single_completion_negated,
                 result_output => Ko );
