c15acc4117adda11aed334566ae331c7247ecdde
Jacob Tilger <jacob.2015@tilger.net>
2026-03-12 08:37:22 +0100

    fixed constraints

--- scripts/set_constraints.tcl
+++ scripts/set_constraints.tcl
@@ -1,48 +1,40 @@
-#write_checkpoint -force /tmp/checkpoint.dcp
+write_checkpoint -force /tmp/checkpoint.dcp
 
 set ack [get_cells -leaf -filter "NCL_WIRE_TYPE == ACK"]
 set bridge [get_cells -leaf -filter "NCL_WIRE_TYPE == NCL_CLK"]
 
+set ncl_gates [get_cells -hierarchical NCL_GATE*]
+
 foreach cc $ack {
 	# find appropriate pins
-	set mark_i [get_pins -of $cc -filter "DIRECTION == IN"]
-	set mark_o [get_pins -of $cc -filter "DIRECTION == OUT"]
-	
-	set ack_snk [get_pins -of [get_nets -segments -of $mark_o] -filter "IS_LEAF && DIRECTION == IN"]
-	set ack_src [get_pins -of [get_nets -segments -of $mark_i] -filter "IS_LEAF && DIRECTION == OUT"]
+	set mark [get_pins -of $cc -filter "DIRECTION == IN"]
+	set net  [get_nets -segments -of $mark]
 	
 	# remove marker
-	set fani [get_nets -of $mark_i]
-	set fano [get_nets -of $mark_o]
-	set fani_pins [get_pins -of $fani -filter "PARENT_CELL != $cc"]
-	
-	remove_net $fani
-	connect_net -net $fano -objects $fani_pins
-	
 	set_property DONT_TOUCH false $cc
 	remove_cell $cc
 	
-	# add constraints 
-	set_min_delay 1.2 -from $ack_src -to $ack_snk
+#	set ack_snk [get_pins -of $net -filter "IS_LEAF && DIRECTION == IN"]
+	set ack_src [get_pins -of $net -filter "IS_LEAF && DIRECTION == OUT"]
 	
-	group_path -name "NCL_STAGE" -from $ack_src -to $ack_snk
+	set pc [get_cells -of $ack_src]
+	
+	set ack_snk [get_pins -of $ncl_gates -filter "PARENT_CELL != $pc && DIRECTION == IN"]
+	
+	# add constraints 
+	set_min_delay 1.5 -from $ack_src -to $ack_snk
+	
+	group_path -name "NCL_ACK_FB" -from $ack_src -to $ack_snk
 }
 
 foreach cc $bridge {
 	# find appropriate pins
-	set mark_i [get_pins -of $cc -filter "DIRECTION == IN"]
-	set mark_o [get_pins -of $cc -filter "DIRECTION == OUT"]
+	set mark [get_pins -of $cc -filter "DIRECTION == IN"]
+	set net  [get_nets -of $mark]
 	
 	# remove marker
-	set fani [get_nets -of $mark_i]
-	set fano [get_nets -of $mark_o]
-	set fani_pins [get_pins -of $fani -filter "PARENT_CELL != $cc"]
-	
-	remove_net $fani
-	connect_net -net $fano -objects $fani_pins
-	
 	set_property DONT_TOUCH false $cc
 	remove_cell $cc
 	
-	set_false_path -setup -hold -rise -fall -through $fano
+	set_false_path -setup -hold -rise -fall -through $net
 }
